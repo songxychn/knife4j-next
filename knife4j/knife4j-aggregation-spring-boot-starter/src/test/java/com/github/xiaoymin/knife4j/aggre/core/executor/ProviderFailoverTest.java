62df7f1312eed28ec072cac266d78d908101869b
worker <worker@knife4j>
2026-04-28 20:44:23 +0000

    style: fix spotless violation in ProviderFailoverTest.java

--- knife4j/knife4j-aggregation-spring-boot-starter/src/test/java/com/github/xiaoymin/knife4j/aggre/core/executor/ProviderFailoverTest.java
+++ knife4j/knife4j-aggregation-spring-boot-starter/src/test/java/com/github/xiaoymin/knife4j/aggre/core/executor/ProviderFailoverTest.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.github.xiaoymin.knife4j.aggre.core.executor;
 
 import com.github.xiaoymin.knife4j.aggre.core.RouteRequestContext;
