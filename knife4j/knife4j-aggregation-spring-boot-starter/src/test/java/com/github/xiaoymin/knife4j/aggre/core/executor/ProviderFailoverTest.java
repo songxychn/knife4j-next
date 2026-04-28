9a415c29e05ea1404554fa2c81356fe6b88570de
worker <worker@knife4j>
2026-04-28 20:46:43 +0000

    style: fix spotless blank lines after license header

--- knife4j/knife4j-aggregation-spring-boot-starter/src/test/java/com/github/xiaoymin/knife4j/aggre/core/executor/ProviderFailoverTest.java
+++ knife4j/knife4j-aggregation-spring-boot-starter/src/test/java/com/github/xiaoymin/knife4j/aggre/core/executor/ProviderFailoverTest.java
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+
 package com.github.xiaoymin.knife4j.aggre.core.executor;
 
 import com.github.xiaoymin.knife4j.aggre.core.RouteRequestContext;
